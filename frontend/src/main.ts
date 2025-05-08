bc30b6a4b3627262825dc29b236aebcc1acbf7ff
jamesgawn <james@gawn.uk>
2025-05-08 23:09:18 +0100

    Move the connection store to the top level

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useEDDLServerStore } from '@/stores/eddl'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +10,10 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+
+const eddlServerConnectionStsore = useEDDLServerStore()
+eddlServerConnectionStsore.bindEvents()
+
 app.use(router)
 
 app.mount('#app')
