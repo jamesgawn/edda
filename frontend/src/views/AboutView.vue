041847cf2a52c996789ac40ffd055a2a36afb671
jamesgawn <james@gawn.uk>
2025-05-09 23:15:37 +0100

    Create initial draft of about page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,23 +1,52 @@
-<script setup>
-import { useEDDLServerStore } from '@/stores/eddl'
-
-const eddlServerConnectionStsore = useEDDLServerStore()
-eddlServerConnectionStsore.bindEvents()
-</script>
+<script setup></script>
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <p class="text-orange-500">{{ eddlServerConnectionStsore.$state.isConnected }}</p>
+  <div class="mb-2.5 text-white md:flex md:items-center md:justify-between">
+    <div class="min-w-0 flex-1">
+      <h2 class="text-2xl/7 font-bold sm:truncate sm:text-3xl sm:tracking-tight">About</h2>
+    </div>
   </div>
+  <main class="flex h-full flex-col text-white">
+    <div class="flex-auto">
+      <p class="mb-2.5">
+        Welcome to the Elite Dangerous Discovery Logbook service. This is an unofficial fan service
+        to provide realtime insights into the exploration activity within the game
+        <a
+          class="text-orange-400 hover:underline"
+          href="https://elitedangerous.com"
+          alt="Go to the Elite Dangerous game website"
+          >Elite: Dangerous</a
+        >.
+      </p>
+      <p class="mb-2.5">
+        The data to provide is service is sourced from the Elite Dangerous Data Network (EDDN),
+        which collects data from volunteer commanders across the galaxy. If you'd like to support
+        this service and others like it, you can contribute your own data, find out more via the
+        <a
+          class="text-orange-400 hover:underline"
+          href="https://github.com/EDCD/EDDN/blob/live/README.md#using-eddn"
+          alt="Go to the Elite Dangerous Data Network website where you can find out how to contribute"
+          >EDDN website</a
+        >.
+      </p>
+      <p class="mb-2.5 font-extrabold">CMDR Akyan</p>
+    </div>
+    <div class="flex-none">
+      <p class="text-xs text-white italic">
+        This website is not affiliated with Frontier Developments. All information provided is based
+        on publicly available information and data supplied by players via
+        <a
+          class="text-orange-400 hover:underline"
+          href="https://github.com/EDCD/EDDN/blob/live/README.md"
+          title="Elite Dangerous Data Network"
+          >EDDN</a
+        >, and may not be entirely accurate. 'Elite', the Elite logo, the Elite: Dangerous logo,
+        'Frontier' and the Frontier logo are registered trademarks of Frontier Developments plc. All
+        rights reserved. All other trademarks and copyrights are acknowledged as the property of
+        their respective owners.
+      </p>
+    </div>
+  </main>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style scoped></style>
