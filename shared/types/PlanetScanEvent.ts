19fc6f6533e96638fb142281eeb61579fd84f8b0
jamesgawn <james@gawn.uk>
2025-05-11 18:25:42 +0100

    Change backend build so it now triggers on main branch

--- shared/types/PlanetScanEvent.ts
+++ shared/types/PlanetScanEvent.ts
@@ -1,5 +1,27 @@
 import type { GenericEvent } from "./GenericEvent";
 
+enum PlanetClass {
+  MetalRich = "Metal rich body",
+  HighMetalContent = "High metal content body",
+  Rocky = "Rocky body",
+  Icy = "Icy body",
+  RockyIcy = "Rocky ice body",
+  Earthlike = "Earthlike body",
+  WaterWorld = "Water world",
+  AmmoniaWorld = "Ammonia world",
+  WaterGiant = "Water giant",
+  WaterGiantWithLife = "Water giant with life",
+  GasGiantWithWaterBasedLife = "Gas giant with water based life",
+  GasGiantWithAmmoniaBasedLife = "Gas giant with ammonia based life",
+  SudarskyClassI = "Sudarsky class I gas giant",
+  SudarskyClassII = "Sudarsky class II gas giant",
+  SudarskyClassIII = "Sudarsky class III gas giant",
+  SudarskyClassIV = "Sudarsky class IV gas giant",
+  SudarskyClassV = "Sudarsky class V gas giant",
+  HeliumRich = "Helium rich gas giant",
+  HeliumGasGiant = "Helium gas giant",
+}
+
 export interface PlanetScanEvent extends GenericEvent {
   Atmosphere: string;
   AtmosphereType: string;
@@ -14,7 +36,7 @@ export interface PlanetScanEvent extends GenericEvent {
   Eccentricity: number;
   Landable: boolean;
   MassEM: number;
-  PlanetClass: string;
+  PlanetClass: PlanetClass;
   MeanAnomaly: number;
   OrbitalInclination: number;
   OrbitalPeriod: number;
