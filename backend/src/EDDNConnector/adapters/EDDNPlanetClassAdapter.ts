37381c236800e8d0213c807fc1c3fbabace68905
jamesgawn <james@gawn.uk>
2025-05-11 22:01:27 +0100

    Improve naming for planet class

--- backend/src/EDDNConnector/adapters/EDDNPlanetClassAdapter.ts
+++ backend/src/EDDNConnector/adapters/EDDNPlanetClassAdapter.ts
@@ -4,10 +4,48 @@ import {
 } from "../../../../shared/types/PlanetScanEvent";
 
 export function toPlanetClassEnum(planetClass: string): PlanetClass {
-  if (Object.values(PlanetClass).includes(planetClass as PlanetClass)) {
-    return planetClass as PlanetClass;
+  switch (planetClass) {
+    case "Metal rich body":
+      return PlanetClass.MetalRich;
+    case "High metal content body":
+      return PlanetClass.HighMetalContent;
+    case "Rocky body":
+      return PlanetClass.Rocky;
+    case "Icy body":
+      return PlanetClass.Icy;
+    case "Rocky ice body":
+      return PlanetClass.RockyIcy;
+    case "Earthlike body":
+      return PlanetClass.Earthlike;
+    case "Water world":
+      return PlanetClass.WaterWorld;
+    case "Ammonia world":
+      return PlanetClass.AmmoniaWorld;
+    case "Water giant":
+      return PlanetClass.WaterGiant;
+    case "Water giant with life":
+      return PlanetClass.WaterGiantWithLife;
+    case "Gas giant with water based life":
+      return PlanetClass.GasGiantWithWaterBasedLife;
+    case "Gas giant with ammonia based life":
+      return PlanetClass.GasGiantWithAmmoniaBasedLife;
+    case "Sudarsky class I gas giant":
+      return PlanetClass.GasGiantClassI;
+    case "Sudarsky class II gas giant":
+      return PlanetClass.GasGiantClassII;
+    case "Sudarsky class III gas giant":
+      return PlanetClass.GasGiantClassIII;
+    case "Sudarsky class IV gas giant":
+      return PlanetClass.GasGiantClassIV;
+    case "Sudarsky class V gas giant":
+      return PlanetClass.GasGiantClassV;
+    case "Helium rich gas giant":
+      return PlanetClass.GasGiantHeliumRich;
+    case "Helium gas giant":
+      return PlanetClass.GasGiantHelium;
+    default:
+      throw new Error(`Invalid PlanetClass: ${planetClass}`);
   }
-  throw new Error(`Invalid PlanetClass: ${planetClass}`);
 }
 
 export function toSimplifiedPlanetClassEnum(
